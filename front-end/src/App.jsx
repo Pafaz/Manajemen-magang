9c1ad099e19324879914e60d92c831a41e595bd0
DevCode.Id <154581685+MuhammadZaki07@users.noreply.github.com>
2025-03-21 09:22:34 +0700

    update libary css to bootrap

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,10 +1,12 @@
-import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
 function App() {
   return (
-    <>
-      <h1>Hello world</h1>
-    </>
+    <div className="container">
+      <h1 className="text-primary">Hello, Bootstrap!</h1>
+      <button className="btn btn-success">Klik Aku</button>
+    </div>
   );
 }
 

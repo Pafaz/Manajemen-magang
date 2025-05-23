af82166fdffa4a83e3a03a6acafc4bb437311ac4
Pafaz <paundra.afif@gmail.com>
2025-05-23 08:26:01 +0700

    fix rekapCabang no model

--- Back-end/app/Services/RekapCabangService.php
+++ Back-end/app/Services/RekapCabangService.php
@@ -24,6 +24,55 @@ class RekapCabangService
         $this->divisiInterface = $divisiInterface;
     }
 
+        public function getRekapCabang($id)
+    {
+        if (auth()->user()->hasRole('admin')) {
+            $id_cabang = auth()->user()->id_cabang_aktif;
+        } else if($id == null) {
+                        return Api::response(
+                'null',
+                'Masukkan ID Cabang',
+            );
+        } else {
+            $id_cabang = $id;
+        }
+
+        // dd($id_cabang);
+        $total_peserta = $this->magangInterface->countPeserta($id_cabang);
+        $total_admin = $this->adminInterface->getByCabang($id_cabang)->count();
+        $total_mentor = $this->mentorInterface->getAll($id_cabang)->count();
+        $total_divisi = $this->divisiInterface->getAll($id_cabang)->count();
+
+        $pesertaPerDivisi = $this->magangInterface->getMagangPerDivisi($id_cabang);
+        $mentorPerDivisi = $this->mentorInterface->getMentorPerDivisi($id_cabang);
+
+        $rekap = [
+            'total_peserta' => $total_peserta,
+            'total_admin' => $total_admin,
+            'total_mentor' => $total_mentor,
+            'total_divisi' => $total_divisi,
+            'peserta_per_divisi' => $pesertaPerDivisi->map(function ($item) {
+                return [
+                    'id_divisi' => $item->id_divisi,
+                    'nama_divisi' => $item->divisi->nama ?? '-',
+                    'total_peserta' => $item->total,
+                ];
+            }),
+            'mentor_per_divisi' => $mentorPerDivisi->map(function ($item) {
+                return [
+                    'id_divisi' => $item->id_divisi,
+                    'nama_divisi' => $item->divisi->nama ?? '-',
+                    'total_mentor' => $item->total,
+                ];
+            }),
+        ];
+
+        return Api::response(
+            $rekap,
+            'Rekap Cabang berhasil ditampilkan',
+        );
+    }
+
     public function simpanRekap($id = null)
     {
         if (auth()->user()->hasRole('admin')) {
